be131180b2dea4263587e28747eecef374b8f7fa
AJGamesArchive <ajward2003@gmail.com>
2023-10-09 11:49:23 +0100

    Created Base Customer & Account Classes

---
+++ Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Management_System
+{
+    internal class Accounts : Customer
+    {
+        public enum Type
+        {
+            Customer,
+            Admin
+        }
+        public string Password { get; set; }
+        public Type Rank { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Management_System
+{
+    internal class Customer
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool GDPR { get; set; }
+    }
+}
